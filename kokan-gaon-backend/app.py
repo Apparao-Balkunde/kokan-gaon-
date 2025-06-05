baadfb40ab958ead6331d8a9428cab6b5be09155
Apparao-Balkunde <apparaobalkunde901@gmail.com>
2025-06-05 18:44:30 +0530

    fixed debbud

--- kokan-gaon-backend/app.py
+++ kokan-gaon-backend/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)  # Allow frontend access
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Kokan Gaon backend running!"
+    return jsonify({'message': 'Welcome to Kokan Gaon Backend!'})
 
-if __name__ == "__main__":
+if __name__ == 'main':
     app.run(debug=True)
