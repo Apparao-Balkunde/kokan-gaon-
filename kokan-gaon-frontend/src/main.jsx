6866224b8a0a1f9ecbd8487f10cb8bba46c25172
Apparao-Balkunde <apparaobalkunde901@gmail.com>
2025-06-07 18:52:48 +0530

    fix bug

--- kokan-gaon-frontend/src/main.jsx
+++ kokan-gaon-frontend/src/main.jsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './app'; 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <App /> </React.StrictMode> );
+ReactDOM.createRoot(document.getElementById('root')).rend
+er( 
+  <React.StrictMode> 
+        <App /> 
+  </React.StrictMode> 
+);
